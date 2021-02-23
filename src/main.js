7b87f6f5d601b031cea60ad74a4c4e84a8875eeb
L-xiaoli <625529828@qq.com>
2021-02-23 19:36:55 +0800

    引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 引入Vant
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
+Vue.use(Vant)
 new Vue({
   router,
   store,
