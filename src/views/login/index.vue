bd9a5275b6e9e8a208924e59c444d7c2b2e9b55a
L-xiaoli <625529828@qq.com>
2021-02-24 10:10:16 +0800

    实现基本的登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,10 +5,11 @@
     <!-- /导航栏 -->
     <!-- 登录表单 -->
     <van-form @submit="onSubmit">
-      <van-field name="手机号" placeholder="请输入手机号">
+      <van-field name="手机号" v-model="user.mobile" placeholder="请输入手机号">
         <i slot="left-icon" class="iconfont iconshouji"> </i>
       </van-field>
       <van-field
+        v-model="user.code"
         type="password"
         name="验证码"
         left-icon="smile-o"
@@ -32,24 +33,45 @@
 </template>
 
 <script>
+import { login } from '@/api/user.js'
 export default {
-  name: 'LoginPage',
+  name: 'LoginIndex',
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created() {},
   mounted() {},
   methods: {
-    onSubmit(values) {
-      console.log('submit', values)
+    async onSubmit() {
+      // 1、截取表单数据
+      const user = this.user
+      // 2、表单验证
+      // 3、提交表单请求登录
+      try {
+        const res = await login(user)
+        console.log('登录成功', res)
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log('登录手机号验证码错误失败', err)
+        } else {
+          console.log('登陆失败，请稍后重试！', err)
+        }
+      }
+      // 4、根据请求响应结果处理后续操作
     }
   }
 }
 </script>
+
 <style scoped lang="less">
 .iconfont {
   font-size: 37px;
