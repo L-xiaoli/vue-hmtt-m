e19f214d8a0d7fb5306c642f5cc2d8871e28db37
L-xiaoli <625529828@qq.com>
2021-02-28 20:44:44 +0800

    创建组件并配置路由

---
+++ src/views/search/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    搜索页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchIndex',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
