31d4a2b3b2e1f4a0a3b98b94496dac3528fca0ca
L-xiaoli <625529828@qq.com>
2021-02-28 21:33:20 +0800

    头部搜索栏布局

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    搜索页面
+  <div class="search-container">
+    <!-- 在van-search外层增加form标签, 且action不为空,即可IOS输入法显示搜索按钮 -->
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        background="#3296fa"
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
   </div>
 </template>
 
@@ -9,11 +19,26 @@ export default {
   name: 'SearchIndex',
 
   data() {
-    return {}
+    return {
+      value: ''
+    }
   },
 
-  methods: {}
+  methods: {
+    onSearch(val) {
+      this.$toast(val)
+    },
+    onCancel() {
+      this.$toast('取消')
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.search-container {
+  .van-search__action {
+    color: #fff;
+  }
+}
+</style>
