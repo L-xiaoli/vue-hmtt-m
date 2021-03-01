787ee5c33d9b628ae3e17d238a256bb3434b847d
L-xiaoli <625529828@qq.com>
2021-03-01 10:09:55 +0800

    搜索页面的页面布局

---
+++ src/views/search/components/search-suggestion.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="search-suggestion">
+    <van-cell title="搜索联想建议" icon="search" />
+    <van-cell title="搜索联想建议" icon="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchSuggestion',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="search-container">
+    <!-- 搜索栏 -->
     <!-- 在van-search外层增加form标签, 且action不为空,即可IOS输入法显示搜索按钮 -->
     <form action="/">
       <van-search
@@ -11,13 +12,34 @@
         @cancel="onCancel"
       />
     </form>
+    <!-- / 搜索栏 -->
+
+    <!-- 搜索历史记录 -->
+    <search-history></search-history>
+    <!-- / 搜索历史记录 -->
+
+    <!-- 搜索联想建议 -->
+    <search-suggestion></search-suggestion>
+    <!-- / 搜索联想建议 -->
+
+    <!-- 历史结果 -->
+
+    <search-result></search-result>
+    <!-- / 历史结果 -->
   </div>
 </template>
 
 <script>
+import SearchHistory from './components/search-history.vue'
+import SearchSuggestion from './components/search-suggestion.vue'
+import SearchResult from './components/search-result.vue'
 export default {
   name: 'SearchIndex',
-
+  components: {
+    SearchHistory,
+    SearchSuggestion,
+    SearchResult
+  },
   data() {
     return {
       value: ''
