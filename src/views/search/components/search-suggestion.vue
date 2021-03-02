adae94aa85baf2424e47bf1dd89af5af1f7f476d
L-xiaoli <625529828@qq.com>
2021-03-02 09:41:30 +0800

    联想建议-debounce优化防抖

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -11,6 +11,8 @@
 
 <script>
 import { getSearchSuggestion } from '@/api/search.js'
+// lodash 支持按需加载，有利于打包结果优化
+import { debounce } from 'lodash'
 export default {
   name: 'SearchSuggestion',
   props: {
@@ -26,9 +28,9 @@ export default {
   },
   watch: {
     searchText: {
-      handler(searchText) {
+      handler: debounce(function(searchText) {
         this.loadSearchSuggestion(searchText)
-      },
+      }, 1000),
       immediate: true // 该回调将会在侦听开始之后被立即调用
     }
   },
