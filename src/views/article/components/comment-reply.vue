72d7ecbc70cec518dd8093893257d18fc3089675
L-xiaoli <625529828@qq.com>
2021-03-06 15:43:20 +0800

    评论回复-处理底部视图布局

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -9,17 +9,21 @@
       <van-icon slot="left" name="cross" @click="$emit('close')" />
     </van-nav-bar>
     <!-- /导航栏 -->
+    <div class="srcoll-wrap">
+      <!-- 当前评论项 -->
+      <comment-item :comment="comment" />
+      <!-- /当前评论项 -->
 
-    <!-- 当前评论项 -->
-    <comment-item :comment="comment" />
-    <!-- /当前评论项 -->
-
-    <!-- 评论的回复列表 -->
-    <van-cell title="全部回复" />
-    <comment-list :source="comment.com_id" type="c" />
-    <!-- /评论的回复列表 -->
+      <!-- 评论的回复列表 -->
+      <van-cell title="全部回复" />
+      <comment-list :source="comment.com_id" type="c" />
+      <!-- /评论的回复列表 -->
+    </div>
 
     <!-- 底部 -->
+    <div class="post-wrap">
+      <van-button class="post-btn" size="small" round>写评论</van-button>
+    </div>
     <!-- /底部 -->
   </div>
 </template>
@@ -47,4 +51,30 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.srcoll-wrap {
+  position: fixed;
+  top: 10%;
+  margin-top: 92px;
+  left: 0;
+  right: 0;
+  bottom: 88px;
+  overflow-y: auto;
+}
+
+.post-wrap {
+  position: fixed;
+  height: 88px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  .post-btn {
+    width: 60%;
+    border-top: 1px solid #d8d8d8;
+  }
+}
+</style>
